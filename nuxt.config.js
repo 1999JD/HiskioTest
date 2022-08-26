15187e1475053ecf0c1b54924db7af17652c5bfd
tkb0004413 <writer159.new@gmail.com>
2022-08-26 03:28:44 +0000

    feat: 引入Fontawesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,10 +15,15 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '~/assets/css/tailwind.css',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    { src: '~/plugins/fontawesome.js', ssr: true }, // only on client side
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
